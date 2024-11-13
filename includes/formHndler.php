5c17fc3b2b2ccf9ea0e131e354a27b7866355909
Anatoli <onotoliuswork@gmail.com>
2024-11-13 10:35:34 +0200

    fix some problems with handler

--- includes/formHndler.php
+++ includes/formHndler.php
@@ -3,8 +3,13 @@
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
-
+    if (empty($email) || empty($password)) {
+        exit("Ошибка, данные формы не были заполныены");
+        header('Location: ../index.php');
+    }
     echo "This is login: $email" . "<br>";
     echo "This is pass: $password" . "<br>";
-    header('/index.php');
+    header('Location: ../index.php');
+} else {
+    header('Location: ../index.php');
 }
